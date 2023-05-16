9bf4f45ab3d7a2b41e4cbca66f6fffa261ff84d7
Abhishek Jaiswal <contactaj9559@gmail.com>
2023-05-16 22:14:00 +0530

    Updates

---
+++ precache-manifest.e34dc3087eb06d147b684f33f35a933c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe0c26472ceac873d1c6",
+    "url": "/Todo-list-using_react/static/css/main.a3b07748.chunk.css"
+  },
+  {
+    "revision": "fe0c26472ceac873d1c6",
+    "url": "/Todo-list-using_react/static/js/main.fe0c2647.chunk.js"
+  },
+  {
+    "revision": "62d1d3bde410af2e0184",
+    "url": "/Todo-list-using_react/static/css/1.23bd4583.chunk.css"
+  },
+  {
+    "revision": "62d1d3bde410af2e0184",
+    "url": "/Todo-list-using_react/static/js/1.62d1d3bd.chunk.js"
+  },
+  {
+    "revision": "55c47aaf12121c9a005b",
+    "url": "/Todo-list-using_react/static/js/runtime~main.55c47aaf.js"
+  },
+  {
+    "revision": "8641c91aa5288bd7045bfb66ddfc8076",
+    "url": "/Todo-list-using_react/index.html"
+  }
+];
